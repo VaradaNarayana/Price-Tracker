2b846c52e64dfeac6b651eaaadf96b8e35f68175
Varada Narayana <126743340+VaradaNarayana@users.noreply.github.com>
2025-08-06 13:17:23 +0530

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from twilio.rest import Client
+import os
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 11)"
+}
+
+def get_price(url):
+    r = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(r.content, "html.parser")
+    title = soup.find("span", {"class": "B_NuCI"}).text.strip()
+    price = soup.find("div", {"class": "_30jeq3 _16Jk6d"}).text.strip()
+    return title, price
+
+def send_whatsapp(product, price):
+    client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
+    message = client.messages.create(
+        body=f"{product}\nPrice: {price}",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+917996261245"
+    )
+    print("Sent:", message.sid)
+
+if __name__ == "__main__":
+    urls = [
+        "https://www.flipkart.com/as-it-is-nutrition-atom-whey-protein/p/itm7e172d65e6c39?pid=PSLGFZ7TAHXRHUTX",
+        "https://www.flipkart.com/tata-1mg-magnesium-glycinate-tablets-muscle-recovery-sleep-nerve-health/p/itmb5e128d34e67f?pid=VSLHACV4AFCDWNGG"
+    ]
+    for url in urls:
+        title, price = get_price(url)
+        send_whatsapp(title, price)
